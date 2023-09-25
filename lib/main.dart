86284d9ff776dd1e847da383d639286000d844e3
islamAshraf <islamashraf520@gmail.com>
2023-09-25 18:44:33 +0300

    add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:taskes_app/simple_bloc_observer.dart';
 import 'Bloc/add_task_cubit/cubit/add_tasks_cubit.dart';
 import 'Model/task_model.dart';
 import 'Views/tasks_view.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox('Tasks_Box');
   Hive.registerAdapter(TaskModelAdapter());
